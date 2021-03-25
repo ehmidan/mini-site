861f4ab2e72d16d15e6e4450b29a63ffe8202b95
ehmidan <ehmida.soulayman.solicode@gmail.com>
2021-03-25 16:50:49 +0100

    add function to js file to send data

--- mini-project/js/main.js
+++ mini-project/js/main.js
@@ -1,15 +1,16 @@
-function addToJson(){
-    var inputes=document.querySelectorAll("input");
-    var info=[inputes[0].value,inputes[1].value,inputes[3].value];
-    var tags=inputes[2].value;
-    var membres =document.querySelector("textarea").value;
-     var xhttp = new XMLHttpRequest();
-     xhttp.onreadystatechange = function() {
-       if (this.readyState == 4 && this.status == 200) {
+
+function addToJsonFile(){
+  event.preventDefault();
+  var inputes=document.querySelectorAll("input");
+  var memebrs=document.querySelector("textarea");
+  var data=[inputes[0].value,inputes[1].value,inputes[2].value,inputes[3].value,memebrs.value];
+  var xhttp =new XMLHttpRequest();
+  xhttp.onreadystatechange=function(){
+    if (this.readyState==4 && this.status==200){
          console.log(this.responseText);
-       }
-     };
-     xhttp.open("POST", "main.php", true);
-     xhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-     xhttp.send("info="+info+"tags="+tags+"membres="+membres+"");
+    }
+  }
+  xhttp.open("POST","main.php");
+  xhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded")
+  xhttp.send("data="+data+"");
 }
